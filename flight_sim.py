847de9a32e1f4106847097d9ec27910b1a9e3cb5
johnviljoen <johnviljoen2@gmail.com>
2021-09-23 19:01:05 +0100

    I have a flight sim now... excuse me what

--- flight_sim.py
+++ flight_sim.py
@@ -48,24 +48,74 @@ class Cam(Entity):
         camera.fov = 45
         camera.z = -10
         
+def print_states():
+    
+    npos_print = Text(text=f"npos: {f16.x.values[0]}")
+    npos_print.x = -0.5
+    npos_print.y = 0.4
+    
+    epos_print = Text(text=f"epos: {f16.x.values[1]}")
+    epos_print.x = -0.5
+    epos_print.y = 0.35
+    
+    h_print = Text(text=f"h: {f16.x.values[2]}")
+    h_print.x = -0.5
+    h_print.y = 0.30
+    
+    phi_print = Text(text=f"phi: {f16.x.values[3]}")
+    phi_print.x = -0.5
+    phi_print.y = 0.25
+    
+    theta_print = Text(text=f"theta: {f16.x.values[4]}")
+    theta_print.x = -0.5
+    theta_print.y = 0.20
+    
+    psi_print = Text(text=f"psi: {f16.x.values[5]}")
+    psi_print.x = -0.5
+    psi_print.y = 0.15
+    
+    destroy(npos_print, delay=time.dt)
+    destroy(epos_print, delay=time.dt)
+    destroy(h_print, delay=time.dt)
+    destroy(phi_print, delay=time.dt)
+    destroy(theta_print, delay=time.dt)
+    destroy(psi_print, delay=time.dt)
+    
+def destroy_states():
+    pass
+    
+            
 def update():
     dh_cmd = 0
     da_cmd = 0
     dr_cmd = 0
-    txt.text = ""
+    
+    print_states()
+    
     if held_keys['a']:
-        da_cmd = -21.5
-        txt.text = 'a'
-    if held_keys['d']:
         da_cmd = 21.5
+        input_text = Text(text='a')
+        destroy(input_text, delay=.1)
+    if held_keys['d']:
+        da_cmd = -21.5
+        input_text = Text(text='d')
+        destroy(input_text, delay=.1)
     if held_keys['w']:
-        dh_cmd = -25
-    if held_keys['s']:
         dh_cmd = 25
+        input_text = Text(text='w')
+        destroy(input_text, delay=.1)
+    if held_keys['s']:
+        dh_cmd = -25
+        input_text = Text(text='s')
+        destroy(input_text, delay=.1)
     if held_keys['q']:
         dr_cmd = -30
+        input_text = Text(text='q')
+        destroy(input_text, delay=.1)
     if held_keys['e']:
         dr_cmd = 30
+        input_text = Text(text='e')
+        destroy(input_text, delay=.1)
     Thrust_cmd = 2000
     f16.step(np.array([Thrust_cmd, dh_cmd, da_cmd, dr_cmd]))
     if held_keys['esc']:
@@ -73,9 +123,9 @@ def update():
     phi = f16.x.values[3]
     theta = f16.x.values[4]
     psi = f16.x.values[5]
-    f16_body.rotation_z = phi
-    f16_body.rotation_y = theta
-    f16_body_rotation_x = psi
+    f16_body.rotation_z = phi*180/np.pi
+    f16_body.rotation_x = -theta*180/np.pi
+    f16_body_rotation_y = psi*180/np.pi
     # rotate_entity(f16_body)
     # translate_entity(camera)
 
@@ -116,6 +166,10 @@ floor = Floor()
 cam = Cam()
 
 f16.paras.dt = 1/60
+
+
+
+
 
 
 
